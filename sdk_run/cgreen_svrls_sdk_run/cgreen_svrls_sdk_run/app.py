c8e7f5cdccd2e2aadddf3c5ba1c98e5086721b12
Connor Green <cgreen@launchdarkly.com>
2022-03-14 16:23:57 -0500

    table options:

--- sdk_run/cgreen_svrls_sdk_run/cgreen_svrls_sdk_run/app.py
+++ sdk_run/cgreen_svrls_sdk_run/cgreen_svrls_sdk_run/app.py
@@ -3,32 +3,31 @@
 import json
 import time
 import os
+from xml.dom.xmlbuilder import DOMEntityResolver
 import ldclient
 from ldclient.config import Config
+import uuid
+import boto3
 
 print('Starting function')
-
-start_time = time.time()
 
 # Get environment variables
 sdk_key = os.environ.get('SDKKEY_test')
 environ = os.environ.get('env')
-proxy_state = os.environ.get('proxy_state')
+table_name = os.environ.get("table_name")
 
 
-if proxy_state == 'off':
-    ldclient.set_config(Config(sdk_key=sdk_key))
-else:
-    ldclient.set_config(Config(sdk_key=sdk_key,
-        base_uri="http://localhost:8030", #these don't have cloud urls yet, but will eventually be set with env variables as well
-        stream_uri="http://localhost:8030")
-    )
+start_time = time.time()
+
+ldclient.set_config(Config(sdk_key=sdk_key))
 if ldclient.get().is_initialized():
+    sdk_init_time = time.time() - start_time
     print("SDK successfully initialized!")
 else:
     raise Exception("SDK failed to initialize")
 client = ldclient.get()
-print("SDK Init Time: --- %s seconds ---" % (time.time() - start_time))
+print("SDK Init Time: --- %s seconds ---" % sdk_init_time)
+
 
 
 def lambda_handler(event, context):
@@ -36,14 +35,31 @@ def lambda_handler(event, context):
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
-    print("Flag Eval Time: --- %s seconds ---" % (time.time() - start_time))
 
     user = {"key": "user@test.com"}
-
+    flag_start_time = time.time()
     show_feature = client.variation("march-test-flag", user, False)
+    flag_eval_time = time.time() - flag_start_time
+    print("Flag Eval Time: --- %s seconds ---" % flag_eval_time)
     if show_feature:
         print("Feature flag is on")
     else:
         print("Feature flag is off")
+
+    #update dynamoDB table
+    dynamodb = boto3.resource('dynamodb', 'us-east-1')
+
+    table = dynamodb.Table('cgreenSeverlessFlagStore-dev')
+    table.put_item(
+        Item={
+                'runID': str(start_time),
+                'run_type': 'SDK',
+                'sdk_init_timestamp': str(sdk_init_time),
+                'flag_call_timestamp': str(flag_eval_time),
+                'flag': 'march-test-flag',
+            }
+        )
+
+
     return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+
